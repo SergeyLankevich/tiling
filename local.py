231f10c3dd3b553cbb057dd3d64e95f47fa33c94
SergeyLankevich <43179675+SergeyLankevich@users.noreply.github.com>
2020-03-15 23:15:27 +0700

    Create local.py

---
+++ local.py
@@ -0,0 +1,5 @@
+COLOR_CHOICE = 'Pick a color from the following list: Green; Yellow; Red; Blue; Black; Maroon.\n'
+HEXAGONS_NUMBER = 'Type in the number of hexagons you want us to use: '
+COLOR_EXCEPTION = "There's no such color in our assortment. Try again: "
+COLOR_ACCEPTANCE = 'Correct input: color accepted'
+HEXAGONS_EXCEPTION = 'Incorrect input: make sure you type in an integer in range from 4 to 20.'
